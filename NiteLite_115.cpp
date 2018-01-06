a05cb24801018e02b656faab05a3f5715c16eb8b
Jeff Wiedemann <jeff@jeffwiedemann.com>
2018-01-06 22:29:27 +0000

    adds tiff and raw images, changes time to ticks

--- NiteLite_115.cpp
+++ NiteLite_115.cpp
@@ -58,12 +58,14 @@ int main()
     time (&rawtime);
     timeinfo = localtime(&rawtime);
 
-    strftime(buffer,sizeof(buffer),"%d-%m-%Y_%H:%M:%S",timeinfo);
+    strftime(buffer,sizeof(buffer),"%H%M%S",timeinfo);
     std::string image_time(buffer);
 
     std::cout << image_time;
     // end of theft 
 
+	clock_t t; 
+	t = clock();   		
 
     // The exit code of the sample application.
     int exitCode = 0;
@@ -96,24 +98,31 @@ int main()
 	
 		exposure_time = 50000;
 
-		// stolen from github.com/ellenschallig/internship/GrabImage.cpp
-		ostringstream filename;
-		filename << "/opt/pylon5/Samples/C++/NiteLite_115/image_" << image_time << "_" <<  exposure_time << "_" << idx << ".tiff";
-		gcstring tmp = filename.str().c_str();
-		// End of theft
 
+		// stolen from github.com/ellenschallig/internship/GrabImage.cpp
+		ostringstream filename1;
+		filename1 << "image_" << t << "_" <<  exposure_time << "_" << idx << ".raw";
+		gcstring filename_raw = filename1.str().c_str();
+		
+		ostringstream filename2;
+		filename2 << "image_" << t << "_" <<  exposure_time << "_" << idx << ".tiff";
+                gcstring filename_tiff = filename2.str().c_str();
+
+		// End of theft
+		
+		
                 Camera.ExposureTime.SetValue(exposure_time); // in microseconds 
                 if ( Camera.GrabOne( 1000, ptrGrabResult))
                 {
                     // The pylon grab result smart pointer classes provide a cast operator to the IImage
                     // interface. This makes it possible to pass a grab result directly to the
                     // function that saves an image to disk.
-                    //CImagePersistence::Save( ImageFileFormat_Tiff, "GrabbedImage_1.tiff", ptrGrabResult);
-                    CImagePersistence::Save( ImageFileFormat_Tiff, tmp, ptrGrabResult);
-                }
+                    CImagePersistence::Save( ImageFileFormat_Tiff, filename_tiff, ptrGrabResult);
+                    CImagePersistence::Save( ImageFileFormat_Raw, filename_raw, ptrGrabResult);
+  		}
 
 
-		} // end of 50000us exposure idx for loop
+	} // end of 50000us exposure idx for loop
 
 		// take another image set at 100ms
 		for(int idx = 1; idx <= 2; idx++)
@@ -121,9 +130,16 @@ int main()
 		exposure_time=100000;
 
 		// stolen from github.com/ellenschallig/internship/GrabImage.cpp
-		ostringstream filename;
-		filename << "/opt/pylon5/Samples/C++/NiteLite_115/image_" << image_time << "_" <<  exposure_time << "_" << idx << ".tiff";
-		gcstring tmp = filename.str().c_str();
+		ostringstream filename1;
+		filename1 << "image_" << t << "_" <<  exposure_time << "_" << idx << ".raw";
+		gcstring filename_raw = filename1.str().c_str();
+		
+		ostringstream filename2;
+                filename2 << "image_" << t << "_" <<  exposure_time << "_" << idx << ".tiff";
+                gcstring filename_tiff = filename2.str().c_str();
+
+
+
 		// End of theft
 
                 Camera.ExposureTime.SetValue(exposure_time); // in microseconds 
@@ -133,19 +149,26 @@ int main()
                     // The pylon grab result smart pointer classes provide a cast operator to the IImage
                     // interface. This makes it possible to pass a grab result directly to the
                     // function that saves an image to disk.
-                    CImagePersistence::Save( ImageFileFormat_Tiff,tmp , ptrGrabResult);
+                    CImagePersistence::Save(ImageFileFormat_Tiff, filename_tiff, ptrGrabResult);
+                    CImagePersistence::Save(ImageFileFormat_Raw, filename_raw, ptrGrabResult);
                 }
                 } // End of idx for loop
 
 
-		// take one more at 250ms 
+		// take one more at 50ms 
 
 		exposure_time=500000;
 
 		// stolen from github.com/ellenschallig/internship/GrabImage.cpp
 		ostringstream filename;
-		filename << "/opt/pylon5/Samples/C++/NiteLite_115/image_" << image_time << "_" <<  exposure_time << "_" << "1" << ".tiff";
-		gcstring tmp = filename.str().c_str();
+		filename << "image_" << t << "_" <<  exposure_time << "_" << "1" << ".raw";
+		gcstring filename_raw = filename.str().c_str();
+
+		ostringstream filename2;
+                filename2 << "image_" << t << "_" <<  exposure_time << "_" << "1" << ".tiff";
+                gcstring filename_tiff = filename2.str().c_str();
+
+
 		// End of theft
 
                 Camera.ExposureTime.SetValue(exposure_time); // in microseconds 
@@ -155,7 +178,8 @@ int main()
                     // The pylon grab result smart pointer classes provide a cast operator to the IImage
                     // interface. This makes it possible to pass a grab result directly to the
                     // function that saves an image to disk.
-                    CImagePersistence::Save( ImageFileFormat_Tiff,tmp , ptrGrabResult);
+                    CImagePersistence::Save( ImageFileFormat_Tiff, filename_tiff, ptrGrabResult);
+		    CImagePersistence::Save( ImageFileFormat_Raw, filename_raw, ptrGrabResult);
                 }
                 Camera.Close();
             }
